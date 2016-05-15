86b190d0a8af50a902031bf01c3e839b51e1f76f
akash <shariaretanvir_akash@yahoo.com>
2016-05-15 19:09:26 +0600

    rfge

--- AngularPlusWebApi/Controllers/EmployeesAPIController.cs
+++ AngularPlusWebApi/Controllers/EmployeesAPIController.cs
@@ -66,7 +66,7 @@ namespace AngularPlusWebApi.Controllers
                     throw;
                 }
             }
-
+           
             return StatusCode(HttpStatusCode.NoContent);
         }
 
